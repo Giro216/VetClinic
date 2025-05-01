117f3ac21c6bbc2bb5cd906692102e04432a86a4
Giro216 <maks07072004@gmail.com>
2025-05-01 18:17:08 +0700

    Init Recommendation Service

---
+++ RecommendationService/src/test/java/org/vetclinic/recommendationservice/RecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vetclinic.recommendationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
