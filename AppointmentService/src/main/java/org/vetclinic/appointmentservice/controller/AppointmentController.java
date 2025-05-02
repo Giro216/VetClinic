249229359a4fe869fab4854d95fb1554c282ad0f
Giro216 <maks07072004@gmail.com>
2025-05-03 02:30:42 +0700

    Not done yet

--- AppointmentService/src/main/java/org/vetclinic/appointmentservice/controller/AppointmentController.java
+++ AppointmentService/src/main/java/org/vetclinic/appointmentservice/controller/AppointmentController.java
@@ -4,29 +4,41 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.vetclinic.appointmentservice.dto.AppointmentMapper;
+import org.springframework.web.bind.annotation.*;
+import org.vetclinic.appointmentservice.repository.AppointmentRepository;
 import org.vetclinic.appointmentservice.service.AppointmentService;
 import org.vetclinic.appointmentservice.dto.*;
 import org.vetclinic.appointmentservice.model.Appointment;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/appointments")
+@RequestMapping("/appointments")
 public class AppointmentController {
 
     private final AppointmentService service;
+    private final AppointmentRepository appointmentRepository;
 
-    private final AppointmentMapper mapper;
+    @GetMapping("/")
+    public ResponseEntity<List<Appointment>> getAllAppointments() {
+        return ResponseEntity.status(HttpStatus.OK).body(service.getAllAppointments());
+    }
 
-    @PostMapping
+    @PostMapping("/")
     public ResponseEntity<AppointmentResponseDto> createAppointment(
             @RequestBody @Valid AppointmentRequestDto request) {
-        Appointment saved = service.createBooking(mapper.toEntity(request));
-        return ResponseEntity.status(HttpStatus.CREATED).body(mapper.toDto(saved));
+        try {
+            AppointmentResponseDto saved = service.createAppointment(request);
+            return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+    }
+
+    @DeleteMapping("/{appointmentId}")
+    public void deleteAppointment(@PathVariable @Valid Integer appointmentId) {
+        appointmentRepository.deleteById(appointmentId);
     }
 }
 
---
+++ AppointmentService/src/main/java/org/vetclinic/appointmentservice/service/TimeSlotService.java
@@ -0,0 +1,8 @@
+package org.vetclinic.appointmentservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TimeSlotService {
+
+}
