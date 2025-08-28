3fe2f684dcb328520e9540a9d1ccdaa53bb7c5b9
Giro216 <maks07072004@gmail.com>
2025-08-28 22:49:45 +0700

    Delete legacy files

--- settings.gradle.kts
+++
@@ -1,7 +0,0 @@
-rootProject.name = "VetClinic"
-
-//include("ProfileService")
-//include("gateway-service")
-//include("appointment-service")
-//include("lab-service")
-//include("emergency-service")
